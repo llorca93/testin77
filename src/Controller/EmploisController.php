7de0544c3673359ad4ec231656f0c96bfa4b13e4
llorca93 <p.llorca.pl@gmail.com>
2022-01-12 11:47:32 +0100

    emplois update

--- src/Controller/EmploisController.php
+++ src/Controller/EmploisController.php
@@ -31,38 +31,9 @@ class EmploisController extends AbstractController
     public function detail(EmploisRepository $emploisRepository, $slug, MailerInterface $mailer, Request $request)
     {
         $emploi = $emploisRepository->findOneBy(['slug' => $slug]);
-
-        if(!$emploi){
-            throw new NotFoundHttpException('Pas d\'offre trouvée');
-        }
-
-        $form = $this->createForm(ContactEmploiType::class);
-        $contact = $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-
-            
-           
-            $email = (new TemplatedEmail())
-                ->from($contact->get('email')->getData())
-                ->to('77@testingenierie.fr')
-                ->subject('Candidature pour le poste de "' . $emploi->getTitre() . '"')
-                ->htmlTemplate('emails/contact_emploi.html.twig')
-                ->context([
-                    'emploi' => $emploi,
-                    'mail' => $contact->get('email')->getData(),
-                    'message' => $contact->get('message')->getData(),
-                ]);
-            $mailer->send($email);
-
-            $this->addFlash('message', 'Votre e-mail a bien été envoyé');
-            return $this->redirectToRoute('emplois_detail', ['slug' => $emploi->getSlug()]);
-        }
-
         
         return $this->render('emplois/detail.html.twig', [
             'emploi' => $emploi,
-            'form' => $form->createView()
         ]);
     }
 }
